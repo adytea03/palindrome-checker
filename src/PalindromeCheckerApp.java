772d45feb1577df8f4a36893cb5d5887f262dbd0
adytea03 <aditijainxgr1@gmail.com>
2026-03-02 10:01:03 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -7,29 +7,9 @@ import java.util.Scanner;
 
 public class PalindromeCheckerApp {
     public static void main(String[] var0) {
-        Scanner var1 = new Scanner(System.in);
-        System.out.print("Enter a word to check if it's a palindrome: ");
-        String var2 = var1.nextLine();
-        if (isPalindrome(var2)) {
-            System.out.println(var2 + " is a palindrome.");
-        } else {
-            System.out.println(var2 + " is not a palindrome.");
-        }
-
-        var1.close();
-    }
-
-    public static boolean isPalindrome(String var0) {
-        int var1 = 0;
-
-        for(int var2 = var0.length() - 1; var1 < var2; --var2) {
-            if (var0.charAt(var1) != var0.charAt(var2)) {
-                return false;
-            }
-
-            ++var1;
-        }
-
-        return true;
+         System.out.println(" Welcome to PalindromeChecker App  ");
+        System.out.println("Application Name   : PalindromeChecker");
+        System.out.println("Application Version: 1.0.0");
+        System.out.println("This application validates whether a given string is a palindrome.");
     }
 }
