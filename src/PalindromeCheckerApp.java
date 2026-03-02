8a7de847cc262ee25fd775fb575bdf93ef10889c
adytea03 <aditijainxgr1@gmail.com>
2026-03-02 10:47:56 +0530

    UC5: Stack-Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -6,36 +6,31 @@
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
-
         public static void main(String[] args) {
+                 String originalString = "noon";
 
-            // Original string
-            String originalString = "radar";
+                    // Use full package name instead of import
+                    java.util.Stack<Character> stack = new java.util.Stack<>();
 
-            // Convert string to character array
-            char[] charArray = originalString.toCharArray();
+                    // Push characters into stack
+                    for (int i = 0; i < originalString.length(); i++) {
+                        stack.push(originalString.charAt(i));
+                    }
 
-            // Two-pointer approach
-            int start = 0;
-            int end = charArray.length - 1;
+                    boolean isPalindrome = true;
 
-            boolean isPalindrome = true;
+                    // Pop and compare
+                    for (int i = 0; i < originalString.length(); i++) {
+                        if (originalString.charAt(i) != stack.pop()) {
+                            isPalindrome = false;
+                            break;
+                        }
+                    }
 
-            // Compare characters from start and end
-            while (start < end) {
-                if (charArray[start] != charArray[end]) {
-                    isPalindrome = false;
-                    break;
+                    if (isPalindrome) {
+                        System.out.println("The given string \"" + originalString + "\" is a Palindrome.");
+                    } else {
+                        System.out.println("The given string \"" + originalString + "\" is NOT a Palindrome.");
+                    }
                 }
-                start++;
-                end--;
             }
-
-            // Display result
-            if (isPalindrome) {
-                System.out.println("The given string \"" + originalString + "\" is a Palindrome.");
-            } else {
-                System.out.println("The given string \"" + originalString + "\" is NOT a Palindrome.");
-            }
-        }
-    }
